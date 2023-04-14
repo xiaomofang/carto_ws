5ca0cc65a25c62aaa8e0f97a7ef81552c143edde
xiaomofang <2206635207@qq.com>
2023-04-15 04:13:28 +0800

    first commit

---
+++ src/cartographer_ros/cartographer_ros/configuration_files/revo_lds_localization.lua
@@ -0,0 +1,12 @@
+include "revo_lds.lua"
+
+-- TRAJECTORY_BUILDER.pure_localization_trimmer = {
+--     max_submaps_to_keep = 3, -- 最大保存子图数，存定位模式通过子图进行定位，但只需要当前和上一个子图即可，我这里设置的是2
+--   }
+TRAJECTORY_BUILDER.pure_localization_trimmer = {
+    max_submaps_to_keep = 3,
+}
+POSE_GRAPH.optimize_every_n_nodes = 80
+
+return options
+
